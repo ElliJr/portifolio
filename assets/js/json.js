bd8812bbd2768c9bac1d3b149484c63a8393d161
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-12-15 15:53:53 -0300

    update

--- assets/js/json.js
+++ assets/js/json.js
@@ -10,13 +10,8 @@ function carregar(){
 
                 const img = document.createElement("img");
                 img.src = dado.imagem
-                img.alt = dado.id
 
-                const id = document.createElement("p")
-                id.textContent = `ID: ${dado.id}`
-
-                card.appendChild(img)
-                card.appendChild(id)
+                card.appendChild(img)                
                 container.appendChild(card)
             })
         })
